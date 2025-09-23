85585489e764cd708a883cfd054801a117f0b932
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-22 19:54:24 -0700

    Randomized the values of the Color objects

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 // Libraries
 #include <iostream>
+#include <vector>
 #include <cstdlib>   // for rand(), srand()
 #include <ctime>     // for time()
 #include "Color.h"
@@ -14,12 +15,39 @@ const int MAX_COLOR_VALUE = 0;
 const int MIN_COLOR_VALUE = 255;
 
 // Function prototypes
-void displayHeader();      // Display a header before outputting the colors
+void displayHeader();          // Display a header before outputting the Color objects
 
 // Main function
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Declare a vector to hold all the Color objects
+    vector<Color> colors;
+
+    // Call srand and time to create a seed
+    srand(time(0));
+
+    // Randomly create 10 Color objects
+    for (int i = 0; i < NUMBER_OF_COLORS; i++)
+    {
+        // Create a temporary color object
+        Color temp;
+
+        // Set the values via the mutators
+        temp.setRed(rand() % 256);        // Randomize a red value
+        temp.setGreen(rand() % 256);      // Randomize a green value
+        temp.setBlue(rand() % 256);       // Randomize a blue value
+
+        // Push the object into the vector
+        colors.push_back(temp);
+    }
+
+    // Iterate through the vector and display all the Color objects
+    for (int i = 0; i < colors.size(); i++)
+    {
+        // Display the values of the Color objects via the print() method
+        cout << "Color #" << i + 1 << ":" << endl;
+        colors.at(i).print();
+    }
 
     return 0;
 }
@@ -37,10 +65,3 @@ void displayHeader()
     cout << "Red     Green     Blue" << endl;
     cout << "----------------------" << endl;
 }
-
-/*
-    displayStruct()
-    Display all the information of a Restaurant struct.
-    Arguments: a Restaurant struct
-    Returns: none
-*/
