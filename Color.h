0b6f21737dd5697ea29fccfdbd4ed3adc1c12c60
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-22 18:46:21 -0700

    Setting up Lab 14...

---
+++ Color.h
@@ -0,0 +1,11 @@
+// COMSC-210 | Lab 14 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+#pragma once
+
+// Libraries
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Color class
---
+++ main.cpp
@@ -0,0 +1,37 @@
+// COMSC-210 | Lab 14 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+// Libraries
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+// Constants
+
+// Restaurant struct
+
+// Function prototypes
+
+// Main function
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
+
+// Function implementations
+/*
+    consoleInput()
+    Allows the user to enter information for a Restaurant struct.
+    Arguments: none
+    Returns: a Restaurant struct that contains the user's input
+*/
+
+/*
+    displayStruct()
+    Display all the information of a Restaurant struct.
+    Arguments: a Restaurant struct
+    Returns: none
+*/
